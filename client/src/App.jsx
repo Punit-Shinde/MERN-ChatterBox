6beb5bab58ab7b8602c93ae57e05a819da6537b8
Punit Shinde <punitshinde1@gmail.com>
2024-03-05 13:14:51 +0530

    backend done and frontend pages

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "./pages/login/Login"
+import SignUp from "./pages/signup/SignUp";
+import Home from "./pages/home/Home";
+const App = () => {
+  return <div className='p-4 h-screen flex items-center justify-center'>
+  <Home />
+  </div>;
+};
+
+export default App;
